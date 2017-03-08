f8fd4962c01a05b1cffadab77b065090b8292382
magicbrush <magicbrush.lv@gmail.com>
2017-03-09 00:47:09 +0800

    初步完成傅里叶游戏原型

--- 傅里叶游戏/Script/AT振子原型.cs
+++ 傅里叶游戏/Script/AT振子原型.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class AT振子原型 : MonoBehaviour {
 
+	private LineRenderer _LR;
+	public float _ZDepth = 5.0f;
+
 	// Use this for initialization
 	void Start () {
-		
+		_LR = GetComponent<LineRenderer> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Vector3 LPos = transform.localPosition;
+		Vector3 LPos2 = LPos;
+		LPos2.z = _ZDepth;
+		_LR.SetPosition (1, -LPos2);
 	}
 }
