5bbd6ace130a2b5a9725fc9cc9f15614bdefe310
magicbrush <magicbrush.lv@gmail.com>
2017-03-21 21:49:19 +0800

    第六周周一课堂

---
+++ 授课/第六周周一/Script/AT力的实验.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AT力的实验 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate()
+	{
+		Rigidbody rb = GetComponent<Rigidbody> ();
+
+		Vector3 Force = Random.insideUnitSphere;
+
+		rb.AddForce (Force);
+
+	}
+}
