d8725cc088e58ae6db6897e7e8d775030c7801e2
magicbrush <magicbrush.lv@gmail.com>
2017-03-23 01:26:52 +0800

    Oscillator

---
+++ 线描跑酷/Script/ATStrokeRecorder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshCollider))]
+public class ATStrokeRecorder : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseDrag()
+	{
+		MeshCollider mcld = GetComponent<MeshCollider> ();
+		mcld.Raycast(
+	}
+}
