595c943491b321203dd6db073aa82d81ca242179
magicbrush <magicbrush.lv@gmail.com>
2017-03-25 01:46:55 +0800

    ATStrokeRecorder

--- 线描跑酷/Script/ATStrokeRecorder.cs
+++ 线描跑酷/Script/ATStrokeRecorder.cs
@@ -4,8 +4,32 @@ using UnityEngine;
 
 [RequireComponent(typeof(MeshCollider))]
 public class ATStrokeRecorder : MonoBehaviour {
-
 	public Camera _SceneCamera;
+	public int _MouseBtn = 0;
+	public float _StepMin = 0.0001f;
+	private Vector3 _LastPos = new Vector3(float.NegativeInfinity, 0,0);
+	public enum TracePointType
+	{
+		STROKE_START,
+		STROKE_POINT,
+		STROKE_END
+	}
+		
+	[System.Serializable]
+	public class TracePoint
+	{
+		public TracePoint(TracePointType t, Vector3 p)
+		{
+			Type = t;
+			Pos = p;
+		}
+		public TracePointType Type;
+		public Vector3 Pos;
+	}
+
+	public List<TracePoint> _Trace;
+
+	public List<ATStrokeRecorder> _SKRecs = new List<ATStrokeRecorder>();
 
 	// Use this for initialization
 	void Start () {
@@ -19,17 +43,101 @@ public class ATStrokeRecorder : MonoBehaviour {
 		
 	}
 
-	void OnMouseDrag()
+	void OnMouseDown()
 	{
-		//MeshCollider mcld = GetComponent<MeshCollider> ();
-		Vector2 hitUV = Vector2.zero;
-		if (HitTestUVPosition (out hitUV)) {
-			Debug.Log ("hitUV:" + hitUV);
+		bool bMouse = Input.GetMouseButton (_MouseBtn);
+		Vector3 hitUV = Vector3.zero;
+		if (HitTestUVPosition (out hitUV) && bMouse) {
+			
+			Vector3 move = hitUV - _LastPos;
+			Debug.Log ("Movement:" + 10000.0f*move.magnitude);
+			if (move.magnitude > _StepMin) {
+				_Trace.Add (new TracePoint (TracePointType.STROKE_START, hitUV));
+				Debug.Log ("OnMouseDown:" + hitUV);
+			}
 		}
 	}
 
-	bool HitTestUVPosition(out Vector2 uv){
-		uv = new Vector2 (float.NaN, float.NaN);
+	void OnMouseDrag()
+	{
+		Vector3 hitUV = Vector3.zero;
+		if (HitTestUVPosition (out hitUV)) {
+			
+			Vector3 move = hitUV - _LastPos;
+			Debug.Log ("Movement:" + 10000.0f*move.magnitude);
+			if (move.magnitude > _StepMin) {
+				_Trace.Add (new TracePoint (TracePointType.STROKE_POINT, hitUV));
+				_LastPos = hitUV;
+				Debug.Log ("OnMouseDrag:" + hitUV);
+			}
+		}
+	}
+
+	void OnMouseOver()
+	{
+		bool bMouse = Input.GetMouseButton (_MouseBtn);
+		Vector3 hitUV = Vector3.zero;
+		if (HitTestUVPosition (out hitUV) && bMouse) {
+			
+			Vector3 move = hitUV - _LastPos;
+			Debug.Log ("Movement:" + 10000.0f*move.magnitude);
+			if (move.magnitude > _StepMin) {
+				_Trace.Add (new TracePoint (TracePointType.STROKE_POINT, hitUV));
+				_LastPos = hitUV;
+				Debug.Log ("OnMouseOver:" + hitUV);
+			}
+		}
+	}
+
+	void OnMouseUp()
+	{
+		Vector3 hitUV = Vector3.zero;
+		if (HitTestUVPosition (out hitUV)) {
+			
+			Vector3 move = hitUV - _LastPos;
+			Debug.Log ("Movement:" + 10000.0f*move.magnitude);
+			if (move.magnitude > _StepMin) {
+				_Trace.Add (new TracePoint (TracePointType.STROKE_END, hitUV));
+				_LastPos = hitUV;
+				Debug.Log ("OnMouseUp:" + hitUV);
+			}
+		}
+	}
+
+	void OnMouseEnter()
+	{
+		bool bMouse = Input.GetMouseButton (_MouseBtn);
+		Vector3 hitUV = Vector3.zero;
+		if (HitTestUVPosition (out hitUV) && bMouse) {
+			
+			Vector3 move = hitUV - _LastPos;
+			Debug.Log ("Movement:" + 10000.0f*move.magnitude);
+			if (move.magnitude > _StepMin) {
+				_Trace.Add (new TracePoint (TracePointType.STROKE_START, hitUV));
+				_LastPos = hitUV;
+				Debug.Log ("OnMouseEnter:" + hitUV);
+			}
+		}
+	}
+
+	void OnMouseExit()
+	{
+		bool bMouse = Input.GetMouseButton (_MouseBtn);
+		Vector3 hitUV = Vector3.zero;
+		if (HitTestUVPosition (out hitUV) && bMouse) {
+			Debug.Log ("OnMouseExit:" + hitUV);
+			Vector3 move = hitUV - _LastPos;
+			if (move.magnitude > 0.00001f) {
+				_Trace.Add (new TracePoint (TracePointType.STROKE_END, hitUV));
+				_LastPos = hitUV;
+			}
+		}
+	}
+
+	bool HitTestUVPosition(out Vector3 pos){
+		
+		pos = new Vector3 (float.NaN, 0, 0);
+
 		RaycastHit hit;
 		Vector3 cursorPos = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 0.0f);
 		Ray cursorRay=_SceneCamera.ScreenPointToRay (cursorPos);
@@ -40,12 +148,30 @@ public class ATStrokeRecorder : MonoBehaviour {
 			if (meshCollider == null || meshCollider.sharedMesh == null)
 				return false;	
 			
-			uv.x = hit.textureCoord.x;
-			uv.y = hit.textureCoord.y;
+			pos = hit.point;
+
 			return true;
 		}
 		else{	
 			return false;
 		}
+	}
+
+	public void Save(int Slot)
+	{
+		if (Slot >= _SKRecs.Count && Slot<0) {
+			Debug.Log ("Exceed Slot Count");
+			return;
+		}
+		_SKRecs [Slot]._Trace = this._Trace;
+	}
+
+	public void Load(int Slot)
+	{
+		if (Slot >= _SKRecs.Count && Slot<0) {
+			Debug.Log ("Exceed Slot Count");
+			return;
+		}
+		_Trace = _SKRecs [Slot]._Trace;
 	}
 }
