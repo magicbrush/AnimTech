903339a651f0a6e53228f879f73cba8bfe205d43
magicbrush <magicbrush.lv@gmail.com>
2017-03-31 00:45:46 +0800

    更新

---
+++ 线描跑酷/Script/ATPTRun_Servant.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ATPTRun_Servant : MonoBehaviour {
+
+	public GameObject _SpriteObj;
+	public float _Power = 1.0f;
+
+	private ATPT_BrushSprite _BrSp = null;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Rigidbody2D rb = GetComponent<Rigidbody2D> ();
+		rb.velocity *= 0.96f;
+		
+		if (_BrSp == null)
+			return;
+
+		Vector3 posThis = transform.position;
+		Vector3 posBrSp = _BrSp.transform.position;
+
+
+		Vector3 vel = rb.velocity;
+		Vector3 thisToBrSp = (posBrSp - posThis);
+		Vector3 ForceDir = (thisToBrSp - vel).normalized;
+		Vector3 force = _Power * ForceDir;
+
+		//ATTraceRunner Runner = 
+		//	_BrSp.GetComponentInParent<ATTraceRunner> ();
+
+		rb.AddForce (force);
+
+	}
+
+	void OnTriggerStay2D(Collider2D other)
+	{
+		Debug.Log ("Trigger!");
+		if (_BrSp != null)
+			return;
+		
+		ATPT_BrushSprite brs = 
+			other.GetComponent<ATPT_BrushSprite>();
+		if (brs == null)
+			return;
+		_BrSp = brs;
+
+		_SpriteObj.SetActive (true);
+
+		SpriteRenderer sr = GetComponent<SpriteRenderer> ();
+		sr.enabled = false;
+	}
+}
---
+++ 线描跑酷/Script/ATPTRun_Substitute.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ATPTRun_Substitute : MonoBehaviour {
+
+	void OnTriggerStay2D(Collider2D other)
+	{
+		ATPT_BrushSprite brs = 
+			other.GetComponent<ATPT_BrushSprite>();
+		if (brs == null)
+			return;
+
+		SpriteRenderer sr = GetComponent<SpriteRenderer> ();
+		int 长 = sr.sprite.texture.width;
+		int 宽 = sr.sprite.texture.height;
+
+		SpriteRenderer srTgt = 
+			brs.GetComponent<SpriteRenderer> ();
+		srTgt.sprite = Sprite.Create (
+			sr.sprite.texture, 
+			new Rect (0, 0, 长, 宽),
+			0.5f*Vector2.one);
+		srTgt.color = sr.color;
+
+		Destroy (gameObject);
+	}
+}
